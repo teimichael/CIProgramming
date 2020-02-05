61acca5fba29c313a338d75790577538a983065e
teimichael <teimichael@outlook.com>
2020-02-05 23:55:25 +0900

    Update PrintToolbox.

---
+++ src/main/java/stu/napls/ciprogsolu/util/PrintToolbox.java
@@ -0,0 +1,144 @@
+package stu.napls.ciprogsolu.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintToolbox {
+
+    public static void print(int x) {
+        System.out.println(x);
+    }
+
+    public static void print(float x) {
+        System.out.println(x);
+    }
+
+    public static void print(double x) {
+        System.out.println(x);
+    }
+
+    public static void print(long x) {
+        System.out.println(x);
+    }
+
+    public static void print(String x) {
+        System.out.println(x);
+    }
+
+    public static void print(int[] x) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < x.length; i++) {
+            list.add(i);
+        }
+        System.out.print(list);
+    }
+
+    public static void print(float[] x) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < x.length; i++) {
+            list.add(i);
+        }
+        System.out.print(list);
+    }
+
+    public static void print(double[] x) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < x.length; i++) {
+            list.add(i);
+        }
+        System.out.print(list);
+    }
+
+    public static void print(long[] x) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < x.length; i++) {
+            list.add(i);
+        }
+        System.out.print(list);
+    }
+
+    public static void print(String[] x) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < x.length; i++) {
+            list.add(i);
+        }
+        System.out.print(list);
+    }
+
+    public static void print(int[][] x) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> subList;
+        for (int i = 0; i < x.length; i++) {
+            subList = new ArrayList<>();
+            for (int j = 0; j < x[0].length; j++) {
+                subList.add(x[i][j]);
+            }
+            list.add(subList);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void print(float[][] x) {
+        List<List<Float>> list = new ArrayList<>();
+        List<Float> subList;
+        for (int i = 0; i < x.length; i++) {
+            subList = new ArrayList<>();
+            for (int j = 0; j < x[0].length; j++) {
+                subList.add(x[i][j]);
+            }
+            list.add(subList);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void print(double[][] x) {
+        List<List<Double>> list = new ArrayList<>();
+        List<Double> subList;
+        for (int i = 0; i < x.length; i++) {
+            subList = new ArrayList<>();
+            for (int j = 0; j < x[0].length; j++) {
+                subList.add(x[i][j]);
+            }
+            list.add(subList);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void print(long[][] x) {
+        List<List<Long>> list = new ArrayList<>();
+        List<Long> subList;
+        for (int i = 0; i < x.length; i++) {
+            subList = new ArrayList<>();
+            for (int j = 0; j < x[0].length; j++) {
+                subList.add(x[i][j]);
+            }
+            list.add(subList);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static void print(String[][] x) {
+        List<List<String>> list = new ArrayList<>();
+        List<String> subList;
+        for (int i = 0; i < x.length; i++) {
+            subList = new ArrayList<>();
+            for (int j = 0; j < x[0].length; j++) {
+                subList.add(x[i][j]);
+            }
+            list.add(subList);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+
+}
--- src/test/java/stu/napls/ciprogsolu/SimpleTest.java
+++ src/test/java/stu/napls/ciprogsolu/SimpleTest.java
@@ -1,8 +1,36 @@
 package stu.napls.ciprogsolu;
 
 import org.junit.Test;
+import stu.napls.ciprogsolu.util.PrintToolbox;
+
+import java.util.Arrays;
 
 public class SimpleTest {
+
+    // Arrays
+    @Test
+    public void testArrays() {
+        // Clone array
+        int[] a = {3, 2, 1, 4, 5, 6};
+        int[] b = a.clone();
+        b[0] = 100;
+        System.out.println(a[0]);
+        System.out.println(b[0]);
+
+        // Sort array
+        Arrays.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        PrintToolbox.print(array);
+    }
+
 
     @Test
     public void testInt2Byte() {
@@ -14,7 +42,7 @@ public class SimpleTest {
     public void testSth() {
         char a = 'a';
         char b = 'b';
-        System.out.println(String.valueOf(a)+(b));
+        System.out.println(String.valueOf(a) + (b));
 
     }
 }
